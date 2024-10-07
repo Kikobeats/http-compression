5a8fb8192cc9de2e30a1ec301e52c93cdc80c37b
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-10-07 16:34:52 +0200

    fix: compression level is an object

--- benchmark/index.mjs
+++ benchmark/index.mjs
@@ -21,6 +21,18 @@ const ratio = (compressed, buffer) => 100 - (compressed.length / buffer.length *
 
 const efficiency = (compressed, buffer, time) => ratio(compressed, buffer) / time
 
+const mostEfficient = collection => {
+  const index = collection.reduce((bestIndex, current, index, array) => {
+    const best = array[bestIndex]
+    if (current.efficiency > best.efficiency) return index
+    return bestIndex
+  }, 0)
+
+  return collection[index]
+}
+
+const brotliResults = []
+
 for (let level = zlib.constants.BROTLI_MIN_QUALITY - 1; level <= zlib.constants.BROTLI_MAX_QUALITY; level++) {
   const duration = timeSpan()
   const compressed = zlib.brotliCompressSync(buffer, { params: { [zlib.constants.BROTLI_PARAM_QUALITY]: level } })
@@ -34,10 +46,21 @@ for (let level = zlib.constants.BROTLI_MIN_QUALITY - 1; level <= zlib.constants.
     return note.join(' ')
   })()
 
-  console.log(`brotli level=${level} bytes=${compressed.length} end=${end} ratio=${percent(ratio(compressed, buffer))} efficiency=${percent(efficiency(compressed, buffer, end))}\t${annotation}`)
+  const result = { level, bytes: compressed.length, ratio: ratio(compressed, buffer), annotation, efficiency: efficiency(compressed, buffer, end) }
+  brotliResults.push(result)
 }
 
+brotliResults.forEach(({ level, bytes, ratio, efficiency, annotation }) => {
+  console.log(`brotli level=${level} bytes=${bytes} ratio=${percent(ratio)} efficiency=${percent(efficiency)}\t${annotation}`)
+})
+
+const brotliMostEfficient = mostEfficient(brotliResults)
+
 console.log()
+console.log(`preferred brotli level: ${brotliMostEfficient.level} with efficiency=${percent(brotliMostEfficient.efficiency)} and ratio=${percent(brotliMostEfficient.ratio)}`)
+console.log()
+
+const gzipResults = []
 
 for (let level = zlib.constants.Z_MIN_LEVEL; level <= zlib.constants.Z_MAX_LEVEL; level++) {
   const finish = timeSpan()
@@ -53,15 +76,37 @@ for (let level = zlib.constants.Z_MIN_LEVEL; level <= zlib.constants.Z_MAX_LEVEL
     return note.join(' ')
   })()
 
-  console.log(`gzip level=${level} bytes=${compressed.length} end=${end} ratio=${percent(ratio(compressed, buffer))} efficiency=${percent(efficiency(compressed, buffer, end))}\t${annotation}`)
+  const result = { level, bytes: compressed.length, ratio: ratio(compressed, buffer), annotation, efficiency: efficiency(compressed, buffer, end) }
+  gzipResults.push(result)
 }
 
+gzipResults.forEach(({ level, bytes, ratio, efficiency, annotation }) => {
+  console.log(`brotli level=${level} bytes=${bytes} ratio=${percent(ratio)} efficiency=${percent(efficiency)}\t${annotation}`)
+})
+
+const gzipMostEfficient = mostEfficient(gzipResults)
+
 console.log()
+console.log(`preferred gzip level: ${gzipMostEfficient.level} with efficiency=${percent(gzipMostEfficient.efficiency)} and ratio=${percent(gzipMostEfficient.ratio)}`)
+console.log()
+
+const deflateResults = []
 
 for (let level = zlib.constants.Z_MIN_LEVEL; level <= zlib.constants.Z_MAX_LEVEL; level++) {
   const finish = timeSpan()
   const compressed = zlib.deflateSync(buffer, { level })
   const end = finish()
 
-  console.log(`deflate level=${level} bytes=${compressed.length} end=${end} ratio=${percent(ratio(compressed, buffer))} efficiency=${percent(efficiency(compressed, buffer, end))}`)
+  const result = { level, bytes: compressed.length, ratio: ratio(compressed, buffer), efficiency: efficiency(compressed, buffer, end) }
+  deflateResults.push(result)
 }
+
+deflateResults.forEach(({ level, bytes, ratio, efficiency, annotation }) => {
+  console.log(`brotli level=${level} bytes=${bytes} ratio=${percent(ratio)} efficiency=${percent(efficiency)}\t${annotation}`)
+})
+
+const deflateMostEfficient = mostEfficient(deflateResults)
+
+console.log()
+console.log(`preferred gzip level: ${deflateMostEfficient.level} with efficiency=${percent(deflateMostEfficient.efficiency)} and ratio=${percent(deflateMostEfficient.ratio)}`)
+console.log()
